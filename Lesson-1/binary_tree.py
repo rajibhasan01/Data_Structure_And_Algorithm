4f214ec190c2e5db83b6d6e00bb52460bba5493b
rajibhasan01 <hasan.rajib1996@gmail.com>
2022-07-14 12:45:19 +0600

    updated

--- Lesson-1/binary_tree.py
+++ Lesson-1/binary_tree.py
@@ -33,7 +33,7 @@ class TreeNode:
 
 
 def parse_tuple(data):
-    print(data);
+    # print(data);
     if isinstance(data, tuple) and len(data) == 3:
         node = TreeNode(data[1]);
         node.left = parse_tuple(data[0]);
@@ -51,4 +51,34 @@ def parse_tuple(data):
     The chain of recursive calls ends when parse_tuple encounters a number or None as input.
 """
 
+def display_keys(node, space='\t', level=0):
+    # print(node.key if node else None, level);
+
+    # If the node is empty
+    if node is None:
+        print(space*level + '0');
+        return
+
+    # If the node is a leaf
+    if node.left is None and node.right is None:
+        print(space*level + str(node.key));
+        return;
+
+    # If the node has children
+    display_keys(node.right, space, level+1)
+    print(space*level + str(node.key));
+    display_keys(node.left, space, level+1);
+
 tree_tuple = ((1,3,None), 2, ((None, 3, 4), 5, (6,7,8)));
+tree2 = parse_tuple(tree_tuple);
+
+
+# Manually print the tree data
+print(tree2.key);
+print(tree2.left.key, tree2.right.key);
+print(tree2.left.left.key, tree2.left.right, tree2.right.left.key, tree2.right.right.key);
+print(tree2.right.left.right.key, tree2.right.right.left.key, tree2.right.right.right.key);
+
+# Automatically print the tree
+print('The Tree are shown below: ')
+display_keys(tree2, '   ');
